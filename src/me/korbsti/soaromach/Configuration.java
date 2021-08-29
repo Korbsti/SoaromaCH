5ecdeab3936af8a4861a9ecfd83273a0104604b8
Korbsti <73249443+Korbsti@users.noreply.github.com>
2021-08-29 00:07:54 -0400

    Add files via upload

---
+++ src/me/korbsti/soaromach/Configuration.java
@@ -0,0 +1,33 @@
+package me.korbsti.soaromach;
+
+public class Configuration {
+	Main plugin;
+	
+	public void setConfig(Main instance) {
+		this.plugin = instance;
+		plugin.saveDefaultConfig();
+		plugin.getConfig().addDefault("channel-list", "&8[&3CH&8] &3Known channels are: {channels}");
+		plugin.getConfig().addDefault("reloaded", "&8[&3CH&8] &3Reloaded Configuration");
+		plugin.getConfig().addDefault("invalidArgs", "&8[&3CH&8] &3Invalid arguments!");
+		plugin.getConfig().addDefault("noPerm", "&8[&3CH&8] &3No Permission");
+		plugin.getConfig().addDefault("invalidChannel", "&8[&3CH&8] &3Invalid channel!");
+		plugin.getConfig().addDefault("switchedChannel", "&8[&3CH&8] &3Channel set to &2{channel-name}");
+		plugin.getConfig().addDefault("channels.name", "testChannel");
+		plugin.getConfig().addDefault("channels.name.defaultGlobal", "global");
+		plugin.getConfig().addDefault("channels.name.channelUponJoining", "global");
+		plugin.getConfig().addDefault("channels.name.defaultGlobalPermission", "ch.defaultGlobal.use");
+		plugin.getConfig().addDefault("channels.name.enableGlobalMessageFormat", false);
+		plugin.getConfig().addDefault("channels.name.defaultGlobalMessageFormat", "&8{&2Global&8}&f {player} &8--> &3{message}");
+		plugin.getConfig().addDefault("channels.name.testChannel.permission", "ch.use.testChannel");
+		plugin.getConfig().addDefault("channels.name.testChannel.prefix", "&8[&4testChannel&8]");
+		plugin.getConfig().addDefault("channels.name.testChannel.sendRegardlessOfCurrentChannel", true);
+		plugin.getConfig().addDefault("channels.name.testChannel.enableDistanceMessage", false);
+		plugin.getConfig().addDefault("channels.name.testChannel.distanceMessage", 25);
+		plugin.getConfig().addDefault("channels.name.testChannel.messageFormat", "{channel-prefix} {player} &8--> &3{message}");
+		plugin.getConfig().addDefault("channels.name.testChannel.chlistDisplayAll", true);
+		plugin.getConfig().addDefault("channels.name.testChannel.channelExists", true);
+		plugin.getConfig().addDefault("channels.name.enableArgsAsMessage", false);
+		plugin.getConfig().options().copyDefaults(true);
+		plugin.saveConfig();
+	}
+}
---
+++ src/me/korbsti/soaromach/MessageSender.java
@@ -0,0 +1,134 @@
+package me.korbsti.soaromach;
+
+import me.clip.placeholderapi.PlaceholderAPI;
+import org.bukkit.Bukkit;
+import org.bukkit.entity.Entity;
+import org.bukkit.entity.Player;
+
+import net.md_5.bungee.api.ChatColor;
+
+public class MessageSender {
+
+	Main plugin;
+
+	public MessageSender(Main instance) {
+		this.plugin = instance;
+	}
+
+	public String format(String channelPrefix, Player p, String message,boolean isGlobal) {
+		if(channelPrefix == null) {
+			channelPrefix = plugin.getConfig()
+					.getString("channels.name." + plugin.currentChannel.get(p.getName())+ ".prefix");
+		}
+		String format = plugin.getConfig()
+				.getString("channels.name." + plugin.currentChannel.get(p.getName()) + ".messageFormat");
+		if(isGlobal) {
+			format = plugin.getConfig()
+					.getString("channels.name.defaultGlobalMessageFormat");
+			channelPrefix = "";
+		}
+		String format1 = format.replace("{channel-prefix}", channelPrefix);
+		String format2 = format1.replace("{player}", p.getDisplayName());
+		String format3 = format2.replace("{message}", message);
+		if (plugin.hasPlaceholder) {
+			return (PlaceholderAPI.setPlaceholders(p.getPlayer(), format3));
+		}
+		return format3;
+	}
+
+	public void messageChannelSender(Player player, String message, String permission, boolean isGlobal) {
+		String prefixChannel = null;
+		for (Player p : Bukkit.getOnlinePlayers()) {
+			String name = p.getName();
+			if (plugin.currentChannel.get(name) == null) {
+				plugin.currentChannel.put(name, plugin.getConfig().getString("channels.name.defaultGlobal"));
+			}
+			if (p.hasPermission(permission)) {
+				if (!plugin.getConfig().getBoolean(
+						"channels.name." + plugin.currentChannel.get(player.getName()) + ".enableDistanceMessage")) {
+
+					if (permission == plugin.getConfig().getString("channels.name." + plugin.currentChannel.get(player.getName()) + ".permission")) {
+						prefixChannel = plugin.getConfig().getString("channels.name." + plugin.currentChannel.get(player.getName()) + ".prefix");
+					}
+					if (plugin.getConfig().getBoolean("channels.name." + plugin.currentChannel.get(player.getName())
+							+ ".sendRegardlessOfCurrentChannel") == true) {
+						p.sendMessage(
+								ChatColor.translateAlternateColorCodes('&', format(prefixChannel, player, message, isGlobal)));
+					} else {
+						if (plugin.currentChannel.get(p.getName()) == plugin.currentChannel.get(player.getName())) {
+							p.sendMessage(ChatColor.translateAlternateColorCodes('&',format(prefixChannel, player, message, isGlobal)));
+						}
+					}
+
+				} else {
+					Double dis = plugin.getConfig().getDouble(
+							"channels.name." + plugin.currentChannel.get(player.getName()) + ".distanceMessage");
+					Bukkit.getScheduler().runTask(plugin, new Runnable() {
+
+						@Override
+						public void run() {
+							String prefix = null;
+							for (Entity entity : player.getNearbyEntities(dis, dis, dis)) {
+								if (entity instanceof Player) {
+									Player playerd = (Player) entity;
+									if (permission == plugin.getConfig().getString("channels.name."
+											+ plugin.currentChannel.get(player.getName()) + ".permission")) {
+										prefix = plugin.getConfig().getString("channels.name."
+												+ plugin.currentChannel.get(player.getName()) + ".prefix");
+									}
+									if (plugin.getConfig()
+											.getBoolean("channels.name." + plugin.currentChannel.get(player.getName())
+													+ ".sendRegardlessOfCurrentChannel") == true) {
+										playerd.sendMessage(ChatColor.translateAlternateColorCodes('&',
+												format(prefix, player, message, isGlobal)));
+									} else {
+										if (plugin.currentChannel.get(playerd.getName()) == plugin.currentChannel
+												.get(player.getName())) {
+											playerd.sendMessage(
+													ChatColor
+															.translateAlternateColorCodes(
+																	'&', format(
+																			plugin.getConfig()
+																					.getString("channels.name." + plugin.currentChannel.get(player.getName())+ ".prefix"), player, message, isGlobal)));
+										}
+									}
+
+								}
+							}
+							if (permission == plugin.getConfig().getString(
+									"channels.name." + plugin.currentChannel.get(player.getName()) + ".permission")) {
+								prefix = plugin.getConfig().getString(
+										"channels.name." + plugin.currentChannel.get(player.getName()) + ".prefix");
+							}
+							if (plugin.getConfig()
+									.getBoolean("channels.name." + plugin.currentChannel.get(player.getName())
+											+ ".sendRegardlessOfCurrentChannel") == true) {
+								player.sendMessage(
+										ChatColor.translateAlternateColorCodes('&', format(prefix, player, message, isGlobal)));
+							} else {
+								if (plugin.currentChannel.get(player.getName()) == plugin.currentChannel
+										.get(player.getName())) {
+									player.sendMessage(ChatColor.translateAlternateColorCodes('&', format(
+											plugin.getConfig()
+													.getString("channels.name."
+															+ plugin.currentChannel.get(player.getName()) + ".prefix"),
+											player, message, isGlobal)));
+								}
+							}
+							Bukkit.getLogger()
+									.info(ChatColor.translateAlternateColorCodes('&', format(
+											plugin.getConfig()
+													.getString("channels.name."
+															+ plugin.currentChannel.get(player.getName()) + ".prefix"),
+											player, message, isGlobal)));
+						}
+
+					});
+					return;
+				}
+			}
+
+		}
+		Bukkit.getLogger().info(ChatColor.translateAlternateColorCodes('&', format(prefixChannel, player, message, isGlobal)));
+	}
+}
